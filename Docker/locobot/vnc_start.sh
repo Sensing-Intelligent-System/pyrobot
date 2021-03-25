001a86195a40e8882ba6831f6103dfda1863a9c3
Richard Wang <59532952+Richardwang0326@users.noreply.github.com>
2021-03-25 16:52:15 +0800

    Update vnc_start.sh

--- Docker/locobot/vnc_start.sh
+++ Docker/locobot/vnc_start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-Xvfb :1 -screen 0 1600x1200x16 &
-export DISPLAY=:1
-
-x11vnc -display :1 -rfbauth ~/.vnc/passwd -rfbport 5900 -loop -shared
+x11vnc -display :0 -rfbauth ~/.vnc/passwd -rfbport 5900 -loop -shared
