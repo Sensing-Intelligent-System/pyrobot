31911b72f1ced827e6f29fb80e908b206c540d21
jinglinjackychen <a2396660@gmail.com>
2021-04-13 16:12:22 +0800

    update name of docker image

--- Docker/locobot/build.sh
+++ Docker/locobot/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build --rm -t argnctu/sis:locobot .
+docker build --rm -t argsis/sis:locobot .
--- Docker/locobot/docker_join.sh
+++ Docker/locobot/docker_join.sh
@@ -27,7 +27,7 @@ BASH_OPTION=bash
 #     echo "run $3"
 # fi
 
-IMG=argnctu/sis:locobot
+IMG=argsis/sis:locobot
 
 xhost +
 containerid=$(docker ps -aqf "ancestor=${IMG}") && echo $containerid
